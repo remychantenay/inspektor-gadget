9d8134810f0ddcb8b14c4972cb3ce45463c1497f
Francis Laniel <flaniel@linux.microsoft.com>
2022-02-17 17:14:04 +0100

    pkg/gadgets: Add bindsnoop types.
    Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>

---
+++ pkg/gadgets/bindsnoop/types/types.go
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright 2019-2022 The Inspektor Gadget authors
+
+package types
+
+import (
+	eventtypes "github.com/kinvolk/inspektor-gadget/pkg/types"
+)
+
+type Event struct {
+	eventtypes.Event
+
+	Pid       uint32 `json:"pid,omitempty"`
+	Comm      string `json:"comm,omitempty"`
+	Protocol  string `json:"proto,omitempty"`
+	Addr      string `json:"addr,omitempty"`
+	Port      uint16 `json:"port,omitempty"`
+	Options   string `json:"opts,omitempty"`
+	Interface string `json:"if,omitempty"`
+	MountNsId uint64 `json:"mountnsid,omitempty"`
+}
+
+func Base(ev eventtypes.Event) Event {
+	return Event{
+		Event: ev,
+	}
+}
